eec60052ed18b592727dd496cba24d67298916e0
Carlovan <giulio.carlassare@gmail.com>
2019-06-23 18:44:27 +0200

    Fixed saren keymap

--- keyboards/carlovan/saren_macro/keymaps/default/keymap.c
+++ keyboards/carlovan/saren_macro/keymaps/default/keymap.c
@@ -8,15 +8,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[BASE] = KEYMAP(
 		KC_F2, KC_F4, KC_F5, 
-		LT(ARROWS, KC_F6), KC_F7, LT(BASE2, KC_F8)),
+		KC_F6, LT(ARROWS, KC_F7), LT(BASE2, KC_F8)),
 
 	[BASE2] = KEYMAP(
 		KC_F9, KC_F11, KC_F12, 
 		KC_F14, KC_F17, KC_NO),
 
 	[ARROWS] = KEYMAP(
-		KC_LEFT, KC_UP, KC_RIGHT, 
-		KC_NO, KC_DOWN, KC_ENT),
+		KC_UP, KC_DOWN, KC_ENT, 
+		KC_NO, KC_NO, KC_NO),
 };
 
 void matrix_init_user(void) {
