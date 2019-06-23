6ad0a3bb4874bf5111065c843fb5129368e04168
Carlovan <giulio.carlassare@gmail.com>
2019-06-23 19:10:50 +0200

    Added RGB to saren_macro

--- keyboards/carlovan/saren_macro/keymaps/default/keymap.c
+++ keyboards/carlovan/saren_macro/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[ARROWS] = KEYMAP(
 		KC_UP, KC_DOWN, KC_ENT, 
-		KC_NO, KC_NO, KC_NO),
+		RGB_MOD, KC_NO, RGB_TOG),
 };
 
 void matrix_init_user(void) {
